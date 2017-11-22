b508d036d9888e7054c357b3d1832ba466906e82
Steve Witman <stevewitman@gmail.com>
2017-11-21 23:34:37 -0700

    Property binding example

--- src/app/new-component/new-component.component.ts
+++ src/app/new-component/new-component.component.ts
@@ -4,31 +4,16 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-new-component',
   template: `
     <h1>New Component</h1>
-    <p>using inline template ... {{ myObj.gender }}</p>
-    <ul>
-      <li *ngFor="let team of myArr">{{ team }}</li>
-    </ul>
-    <div *ngIf="flag; then tmpl1 else tmpl2"></div>
-    <ng-template #tmpl1>
-      <button>Flag is true</button>
-    </ng-template>
-    <ng-template #tmpl2>
-      <button>Flag is false</button>
-    </ng-template>
+    <img [src]="img1">
+    <button [disabled]="buttonStatus">Button</button>
   `,
   styleUrls: ['./new-component.component.css']
 })
 export class NewComponentComponent implements OnInit {
 
-  flag = false;
+  img1 = "http://lorempixel.com/400/200/";
 
-  myObj = {
-    gender: 'male',
-    age: 99,
-    location: 'earth'
-  }
-
-  myArr = ['Nuggets', 'Thunder', 'Warriors']
+  buttonStatus = false;
 
   constructor() { }
 
